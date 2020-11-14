4e4b890bcd077026fe7f371711c8e05d91f3e45a
teresafranxman97 <teresafranxman97@gmail.com>
2020-11-14 00:02:56 -0600

    imported files to APP js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
+import './dummy-data';
+import Posts from "./components/Posts/Posts"
+import SearchBar from "./components/SearchBar/SearchBar"
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
@@ -16,17 +19,16 @@ const App = () => {
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
   const likePost = postId => {
-    /*
-      This function serves the purpose of increasing the number of likes by one, of the post with a given id.
+  /* This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
-      The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
-      This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
+    The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
+    This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
 
-      Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
-      The callback passed into `map` performs the following logic:
-        - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
-        - otherwise just return the post object unchanged.
-     */
+    Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
+    The callback passed into `map` performs the following logic:
+      - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
+      - otherwise just return the post object unchanged.
+  */
   };
 
   return (
